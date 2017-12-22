c80da24be16fc283766f628f5232e8668c537cc0
chester066 <chester066@yahoo.com.tw>
2017-12-22 18:54:41 +0800

    新增testGetTriangleType測試範例

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -3,6 +3,13 @@
 
 void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
+    assert(getTriangleType(6, 5, 3) ==-1);
+    assert(getTriangleType(3, 5, 4) == 0);
+    assert(getTriangleType(5, 5, 3) == 1);
+    assert(getTriangleType(3, 5, 5) == 1);
+    assert(getTriangleType(3, 5, 4) == 0);
+    assert(getTriangleType(3, 4, 8) ==-2);
+
 }
 
 int main() {
